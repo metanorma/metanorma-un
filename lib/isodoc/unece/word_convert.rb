689f90c5cf31b8a79fdd53d986c579a278737995
Nick Nicholas <opoudjis@optusnet.com.au>
2018-11-10 19:06:27 +1100

    remove redundant code

--- lib/isodoc/unece/word_convert.rb
+++ lib/isodoc/unece/word_convert.rb
@@ -66,35 +66,6 @@ module IsoDoc
         section_break(body)
       end
 
-      def title(isoxml, _out)
-        main = isoxml&.at(ns("//title[@language='en']"))&.text
-        set_metadata(:doctitle, main)
-      end
-
-      def generate_header(filename, dir)
-        return unless @header
-        template = Liquid::Template.parse(File.read(@header, encoding: "UTF-8"))
-        meta = @meta.get
-        meta[:filename] = filename
-        params = meta.map { |k, v| [k.to_s, v] }.to_h
-        File.open("header.html", "w") { |f| f.write(template.render(params)) }
-        @files_to_delete << "header.html"
-        "header.html"
-      end
-
-      def header_strip(h)
-        h = h.to_s.gsub(%r{<br/>}, " ").sub(/<\/?h[12][^>]*>/, "").gsub(/<\/?b>/, "")
-        h1 = to_xhtml_fragment(h.dup)
-        h1.traverse do |x|
-          x.replace(" ") if x.name == "span" &&
-            /mso-tab-count/.match(x["style"])
-          x.remove if x.name == "span" && x["class"] == "MsoCommentReference"
-          x.remove if x.name == "a" && x["epub:type"] == "footnote"
-          x.replace(x.children) if x.name == "a"
-        end
-        from_xhtml(h1)
-      end
-
       ENDLINE = <<~END.freeze
       <v:line id="_x0000_s1026"
  alt="" style='position:absolute;left:0;text-align:left;z-index:251662848;
