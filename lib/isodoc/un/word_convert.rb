515a47333ffa2c9a356733aa55664e2f329fe6cc
Nick Nicholas <opoudjis@gmail.com>
2020-03-09 15:17:02 +1100

    allow for neither preface container nor abstractbox in Word output as abstract destination

--- lib/isodoc/un/word_convert.rb
+++ lib/isodoc/un/word_convert.rb
@@ -144,7 +144,8 @@ module IsoDoc
         foreword = docxml.at("//p[@class = 'ForewordTitle']/..")
         intro = docxml.at("//p[@class = 'IntroTitle']/..")
         abstract = docxml.at("//p[@class = 'AbstractTitle']/..")
-        abstract.parent = (abstractbox || preface_container) if abstract
+        abstract.parent = (abstractbox || preface_container) if abstract &&
+          (abstractbox || preface_container)
         abstractbox and abstract&.xpath(".//p/br")&.each do |a|
           a.parent.remove if /page-break-before:always/.match(a["style"])
         end
