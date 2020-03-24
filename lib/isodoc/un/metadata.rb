e809cd82ee6dd98d96577b1716554e18d71c37e0
Nick Nicholas <opoudjis@gmail.com>
2020-03-24 20:50:26 +1100

    refactor month dates metadata: https://github.com/metanorma/isodoc/issues/90

--- lib/isodoc/un/metadata.rb
+++ lib/isodoc/un/metadata.rb
@@ -82,34 +82,6 @@ set(:logo, File.expand_path(File.join(here, "html", "logo.jpg")))
       def unpublished(status)
         !%w(published withdrawn).include? status.downcase
       end
-
-      def version(isoxml, _out)
-        super
-        revdate = get[:revdate]
-        set(:revdate_monthyear, monthyr(revdate))
-      end
-
-      MONTHS = {
-        "01": "January",
-        "02": "February",
-        "03": "March",
-        "04": "April",
-        "05": "May",
-        "06": "June",
-        "07": "July",
-        "08": "August",
-        "09": "September",
-        "10": "October",
-        "11": "November",
-        "12": "December",
-      }.freeze
-
-      def monthyr(isodate)
-        m = /(?<yr>\d\d\d\d)-(?<mo>\d\d)/.match isodate
-        return isodate unless m && m[:yr] && m[:mo]
-        return "#{MONTHS[m[:mo].to_sym]} #{m[:yr]}"
-      end
-
     end
   end
 end
