aec5a2bc8119eec0e847c3ba54fe644db5277115
Nick Nicholas <opoudjis@gmail.com>
2022-09-06 23:26:51 +1000

    version bump

--- lib/metanorma-un.rb
+++ lib/metanorma-un.rb
@@ -1,7 +1,8 @@
 require "metanorma/un"
 require "asciidoctor"
 require "isodoc/un"
+require "metanorma"
 
-if defined? Metanorma
+if defined? Metanorma::Registry
   Metanorma::Registry.instance.register(Metanorma::UN::Processor)
 end
--- lib/metanorma/un/version.rb
+++ lib/metanorma/un/version.rb
@@ -1,5 +1,5 @@
 module Metanorma
   module UN
-    VERSION = "0.9.7".freeze
+    VERSION = "0.9.8".freeze
   end
 end
