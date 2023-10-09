bda051429d505c609324983431181c72ed4b1941
Nick Nicholas <opoudjis@gmail.com>
2023-10-09 22:36:32 +1100

    rspec

--- spec/metanorma/validate_spec.rb
+++ spec/metanorma/validate_spec.rb
@@ -4,7 +4,7 @@ require "fileutils"
 RSpec.describe Metanorma::UN do
   context "when xref_error.adoc compilation" do
     around do |example|
-      FileUtils.rm_f "spec/assets/xref_error.err"
+      FileUtils.rm_f "spec/assets/xref_error.err.html"
       example.run
       Dir["spec/assets/xref_error*"].each do |file|
         next if file.match?(/adoc$/)
@@ -19,40 +19,38 @@ RSpec.describe Metanorma::UN do
         Metanorma::Compile
           .new
           .compile("spec/assets/xref_error.adoc", type: "un", no_install_fonts: true)
-      end.to(change { File.exist?("spec/assets/xref_error.err") }
+      end.to(change { File.exist?("spec/assets/xref_error.err.html") }
               .from(false).to(true))
     end
   end
 
-   it "Warns of illegal doctype" do
-       FileUtils.rm_f "test.err"
-    Asciidoctor.convert(<<~"INPUT", backend: :un, header_footer: true)
-  = Document title
-  Author
-  :docfile: test.adoc
-  :nodoc:
-  :no-isobib:
-  :doctype: pizza
+  it "Warns of illegal doctype" do
+    FileUtils.rm_f "test.err.html"
+    Asciidoctor.convert(<<~INPUT, backend: :un, header_footer: true)
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+      :no-isobib:
+      :doctype: pizza
 
-  text
-  INPUT
-    expect(File.read("test.err")).to include "pizza is not a legal document type"
+      text
+    INPUT
+    expect(File.read("test.err.html")).to include "pizza is not a legal document type"
+  end
+
+  it "Warns of illegal status" do
+    FileUtils.rm_f "test.err.html"
+    Asciidoctor.convert(<<~INPUT, backend: :un, header_footer: true)
+      = Document title
+      Author
+      :docfile: test.adoc
+      :nodoc:
+      :no-isobib:
+      :status: pizza
+
+      text
+    INPUT
+    expect(File.read("test.err.html")).to include "pizza is not a recognised status"
+  end
 end
-
-it "Warns of illegal status" do
-    FileUtils.rm_f "test.err"
-    Asciidoctor.convert(<<~"INPUT", backend: :un, header_footer: true)
-  = Document title
-  Author
-  :docfile: test.adoc
-  :nodoc:
-  :no-isobib:
-  :status: pizza
-
-  text
-  INPUT
-    expect(File.read("test.err")).to include "pizza is not a recognised status"
-end
-
-end
-
